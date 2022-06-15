cecf0a66ab215923f2ccf7f5cc08bf8f9af7d977
YuliaYuzhakova <JuliaU33@mail.ru>
2022-06-15 16:45:06 +0700

    Создан менеджер Movie и тесты к нему

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,10 @@
+package ru.netology.domain.movie;
+
+public class Movie {
+    private String id;
+    private String imageURL;
+    private String name;
+    private String genre;
+    private boolean premiereTomorrow;
+
+}
